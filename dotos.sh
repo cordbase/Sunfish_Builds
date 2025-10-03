68117b52f0e7a68fbad040be92891a0324b6e1d4
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-03 20:17:14 +0530

    Update dotos.sh

--- dotos.sh
+++ dotos.sh
@@ -10,6 +10,15 @@ repo init -u git://github.com/DotOS/manifest.git -b dot11
 # Repo Sync
 /opt/crave/resync.sh 
 
+# Device tree
+git clone --depth=1 -b lineage-18.1 https://github.com/LineageOS/android_device_google_sunfish.git device/google/sunfish
+
+# Vendor tree
+git clone --depth=1 https://github.com/sourajitk/vendor_google_sunfish.git vendor/google/sunfish
+
+# Kernel tree
+git clone --depth=1 https://github.com/LineageOS/android_kernel_google_sunfish.git kernel/google/sunfish
+
 # Build
 source build/envsetup.sh
 lunch dot_sunfish-user
