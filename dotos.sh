060340f16fc5af9ef059338b09a10540a4bf4287
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-03 21:33:23 +0530

    Update dotos.sh

--- dotos.sh
+++ dotos.sh
@@ -7,6 +7,9 @@ rm -rf prebuilts/clang/host/linux-x86
 # Repo Init
 repo init -u git://github.com/DotOS/manifest.git -b dot11
 
+# Gapps
+git clone --depth 1 --branch gapps https://github.com/cordbase/local_manifest.git .repo/local_manifests
+
 # Repo Sync
 /opt/crave/resync.sh 
 
@@ -21,5 +24,5 @@ git clone --depth=1 https://github.com/LineageOS/android_kernel_google_sunfish.g
 
 # Build
 source build/envsetup.sh
-lunch dot_sunfish-user
+lunch dot_sunfish-userdebug
 make bacon
